c52d2cfb567bcbca163a85c5fcbbae0866126cc0
Aleksei <kilroygood@gmail.com>
2022-10-11 20:59:15 +0300

    it works

--- TestNFTFriends2.sol
+++ TestNFTFriends2.sol
@@ -2,24 +2,44 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts@4.7.3/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.7.3/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts@4.7.3/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
 import "@openzeppelin/contracts@4.7.3/utils/Counters.sol";
 
-contract TestNFTFriends2 is ERC721, ERC721Burnable, Ownable {
+contract NFTFriends is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("TestNFTFriends2", "TNT") {}
+    constructor() ERC721("NFTFriends", "TNT2") {}
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://ipfs.io/ipfs/QmQygnxQHZpjjQVQKJpbChP8k971nt3EEPaVcxZHtS9EC3/";
-    }
-
-    function safeMint(address to) public onlyOwner {
+    function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return
+            string(
+                abi.encodePacked(
+                    "https://gateway.pinata.cloud/ipfs/QmYnX9KrZjGvKUnKR9eJ1FYdGN5L1LoqZgXsBqb8BSceHJ/",
+                    Strings.toString(tokenId),
+                    ".json"
+                )
+            );
     }
 }
